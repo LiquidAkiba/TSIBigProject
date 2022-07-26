3320feb749377f68fee2b1da3e4ee581fac7359c
Sean Carr <35836323+LiquidAkiba@users.noreply.github.com>
2022-07-26 17:03:05 +0100

    Add files via upload
    Commit 2; with data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,47 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="*")
 @SpringBootApplication
+@RestController  //handles GET, POST, DELETE, PUT requests
+@RequestMapping("/Sakila")
 public class DemoApplication {
+
+	@Autowired
+	private ActorRepository actorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello() {
-		return "Hello World";
-}
+	public DemoApplication(ActorRepository actorRepository){
+		this.actorRepository = actorRepository;
+	}
 
+
+	//@ResponseBody
+	@PostMapping("/Add_New_Actor")
+	public @ResponseBody String addNewActor(@RequestParam String first_name, @RequestParam String last_name){
+		Actor a = new Actor(first_name, last_name);
+		System.out.println(first_name + " " + last_name);
+		actorRepository.save(a);
+		return "saved";
+	}
+
+	//Get request / read function
+	@GetMapping("/All_Actors")
+	public @ResponseBody
+	Iterable<Actor>getAllActors(){
+		return actorRepository.findAll();
+	}
+
+	@DeleteMapping("/Delete_Actor_By_Id")
+	public @ResponseBody
+	void deleteActorById(@RequestParam int id){
+		actorRepository.deleteById(id);
+	}
 }
